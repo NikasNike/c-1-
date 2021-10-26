293927201b365f1418e7e5a78174c0a912cb1734
Nikas Nike <Nikasnike759@gmail.com>
2021-10-26 22:14:49 +0300

    приветствие

---
+++ exzample005/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "nikita")
+{
+    Console.WriteLine("Ура, это же nikita");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
