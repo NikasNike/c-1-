a465a9b22a084d34eaddc6165a65e20a08fc09b7
Nikas Nike <Nikasnike759@gmail.com>
2021-10-31 19:10:17 +0300

    нахождние числа в массиве

---
+++ exzample10/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {100, 52, 83, 46, 15, 76, 76, 278, 99 };
+
+int n = array.Length;
+int find = 76;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+   
+    index++;
+}
